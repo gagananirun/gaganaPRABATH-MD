8186705a737f8970db4b98d4d2bc0c190f3c2126
gagananirun <167496888+gagananirun@users.noreply.github.com>
2024-05-27 11:50:51 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -24,5 +24,5 @@ SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~478WzYQB#S1CYi5NMa3OUqNh7vNq7e
 BOT_NUMBER: process.env.BOT_NUMBER || "94729493354",
 PASSWORD: process.env.PASSWORD || "Enter you like password",
 GITHUB_USERNAME: process.env.GITHUB_USERNAME || "gagananirun",
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || ""
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "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"
 };
